bb53351d8f67224dbcde17cf36e33a99fa7b48e5
Nick Howard <howardnrh@gmail.com>
2026-04-03 12:47:55 -0700

    preset to list, plus 3 lists

---
+++ convert.lisp
@@ -0,0 +1,35 @@
+(defparameter *puzzle* 
+ '(5 0 7 6 0 0 0 8 9
+   6 0 0 2 0 7 4 0 0
+   9 0 0 3 5 0 0 0 6
+   7 6 3 4 0 2 0 0 0
+   0 8 4 0 0 0 6 3 7
+   0 0 0 7 0 6 8 4 0
+   0 7 1 0 2 4 0 0 0
+   0 0 0 0 7 0 9 0 8
+   0 2 0 0 0 5 1 7 0)
+)
+
+
+; works
+(defun make-rows (puzzle)
+  (loop for i from 0 below 81 by 9
+        collect (subseq puzzle i (+ i 9))))
+(format t "Rows: ~A~%" (make-rows *puzzle*))
+
+; works
+(defun make-cols (rows)
+  (loop for c from 0 below 9
+        collect
+          (loop for r from 0 below 9
+                collect (nth c (nth r rows)))))
+(format t "~%Cols: ~A~%" (make-cols (make-rows *puzzle*)))
+
+; works
+(defun make-grids (rows)
+  (loop for gr from 0 below 3 append
+        (loop for gc from 0 below 3 collect
+              (loop for r from (* gr 3) below (+ (* gr 3) 3) append
+                    (loop for c from (* gc 3) below (+ (* gc 3) 3)
+                          collect (nth c (nth r rows)))))))
+(format t "~%Grids: ~A~%" (make-grids (make-rows *puzzle*)))
--- transformer.lisp
+++ transformer.lisp
@@ -1,10 +1,16 @@
+(defparameter *puzzle* 
+  (list)
+)
+
 (defun filePrint (filename)
   (with-open-file (in filename)
     (when in 
       (loop for char = (read-char in nil)
         while char 
-        do (format t "~c" char))
-      (format t "~%")
+        for digit = (digit-char-p char)
+        when digit
+        do (setf *puzzle* (append *puzzle* (list digit))))
     )))
 
 (filePrint "easyPreset.txt")
+(format t "~A~%" *puzzle*)
